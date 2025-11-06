207ef21b29be343f8b88ab6e387473332b633c8e
Kevin Hill <kevinkhill@pm.me>
2025-11-05 17:53:40 -0800

    renaming output dir

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -3,6 +3,7 @@ import { addOpenSCADPlugin } from "eleventy-plugin-scad";
 /** @param {import("@11ty/eleventy/UserConfig").default} eleventyConfig */
 export default function (eleventyConfig) {
     eleventyConfig.setInputDirectory("models");
+    eleventyConfig.setOutputDirectory("docs");
 
     addOpenSCADPlugin(eleventyConfig, {
         launchPath: "nightly",
